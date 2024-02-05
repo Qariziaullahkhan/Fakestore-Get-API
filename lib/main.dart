d841098727fa0dd8607ae219cf55fb868e7b0292
Malik Zia <malikziakhan0315@gmail.com>
2024-02-05 10:45:02 +0500

    we can create Get Api in flutter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:session14_fakestore/screens/single_product_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SingleProductScreen(),
+    );
+  }
+}
---
+++ lib/screens/single_product_screen.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:gap/gap.dart';
+import 'package:session14_fakestore/widgets/product_card.dart';
+
+import '../models/product.dart';
+import 'package:http/http.dart' as http;
+
+class SingleProductScreen extends StatefulWidget {
+  const SingleProductScreen({super.key});
+
+  @override
+  State<SingleProductScreen> createState() => _SingleProductScreenState();
+}
+
+class _SingleProductScreenState extends State<SingleProductScreen> {
+  Future<Product> getSingleProduct() async {
+    String url = 'https://fakestoreapi.com/products/2';
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body);
+
+      Product product = Product.fromJson(jsonResponse);
+      return product;
+    } else {
+      throw Exception('SWR');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        centerTitle: true,
+        title: const Text(
+          'Product',
+          style: TextStyle(
+              fontSize: 30, color: Colors.pink, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: FutureBuilder<Product>(
+        future: getSingleProduct(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('SWR'),
+            );
+          } else if (snapshot.hasData) {
+            Product product = snapshot.data!;
+
+            return ProductCard(product: product);
+          } else {
+            return const Center(
+              child: SpinKitChasingDots(
+                color: Colors.purple,
+                size: 100,
+                duration: Duration(seconds: 10),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
